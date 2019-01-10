bdf4176d4e3544e1d4e3beff07e2d3631c7abbea
donaisore <r.h.kobe0706@gmail.com>
2019-01-10 18:28:26 +0900

    メールを送信できるようになる。ステップアップ問題。

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,7 @@ class UserMailer < ApplicationMailer
     @blog = params[:blog]
     @entry = params[:entry]
     @comment = params[:comment]
+    @url = blog_entry_url(@blog.id, @entry.id)
     mail(to: "example@sample.com", subject: "新しいコメントが投稿されました。")
   end
 end
